8c1630e7ee456e7c84550e3c48b114637bc87d6f
kiwook <kiwookiwook@gmail.com>
2018-01-05 00:09:17 -0800

    added modules needed for redux + in the process of creating store and modifying index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import App from "./components/App";
 import '../../styles/styles.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+const middleware = applyMiddleware(thunk, logger());
+// need a reducer
+// const store = createStore(middleware)
+
+render(
+  <Provider store = {store} >
+    <App />
+  </Provider>, document.getElementById('app')
 )

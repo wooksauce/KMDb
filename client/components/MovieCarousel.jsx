199684ae3a20c4f7fe2181bea5fe4baa5a246443
kiwook <kiwookiwook@gmail.com>
2018-01-15 16:09:16 -0800

    carousel pictures switching now

--- client/components/MovieCarousel.jsx
+++ client/components/MovieCarousel.jsx
@@ -3,28 +3,43 @@ import React, { Component } from 'react';
 class MovieCarousel extends Component {
   constructor(props) {
     super(props)
-
+    this.state = {
+      carouselPos: 0,
+    }
+    this.clickPrev=this.clickPrev.bind(this);
+    this.clickNext=this.clickNext.bind(this);
   }
+
+  clickPrev() {
+    if (this.state.carouselPos > 0) {
+      this.setState({carouselPos: this.state.carouselPos - 1})
+    } else {
+      this.setState({carouselPos: 2});
+    }
+  };
+
+  clickNext() {
+    if (this.state.carouselPos < 2) {
+      this.setState({carouselPos: this.state.carouselPos + 1})
+    } else {
+      this.setState({carouselPos: 0});
+    }
+  };
 
   render() {
     const { carousel } = this.props;
     if (!carousel || !carousel.length) {
       return null
     }
-    const carouselImgs = makeCarousels(carousel);
+    console.log('pos', this.state.carouselPos)
+    let slide = carousel[this.state.carouselPos];
     return(
       <div className="carouselContainer">
-        {carouselImgs}
-      </div>
-    )
-  }
-}
-
-const makeCarousels = (carouselData = []) => {
-  if (carouselData.length) {
-    return carouselData.map((carousel) =>
-      <div className="carouselImgContainer">
-        <img className="carouselImg" src={carousel.poster} height="180px" />
+        <div onClick={()=>this.clickPrev()}> Prev </div>
+        <div className="carouselImgContainer">
+          <img className="carouselImg" src={slide.poster} height="180px" />
+        </div>
+        <div onClick={()=>this.clickNext()}> Next </div>
       </div>
     )
   }

7c43ad120e4bdcc58c6e4f7c200f106d5f280c2f
kiwook <kiwookiwook@gmail.com>
2018-01-18 23:16:14 -0800

    got the barebone modal to work using ReactModal but is getting some kind of app element error

--- client/components/MovieEntry.jsx
+++ client/components/MovieEntry.jsx
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import ReactModal from 'react-modal';
 
 export default class MovieEntry extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      showModal: false,
+    }
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
+  handleOpenModal () {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal () {
+    this.setState({ showModal: false });
   }
 
   render() {
@@ -11,7 +25,15 @@ export default class MovieEntry extends Component {
     const posterUrl = (!poster || poster === 'N/A') ? 'https://i5.walmartimages.com/asr/f752abb3-1b49-4f99-b68a-7c4d77b45b40_1.39d6c524f6033c7c58bd073db1b99786.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF' : poster;
     return(
       <div className={resultSource + "MovieEntry"}>
-        <div className={resultSource + "MovieEntryContainer"}>
+        <div className={resultSource + "MovieEntryContainer"} onClick={this.handleOpenModal}>
+        <ReactModal
+           isOpen={this.state.showModal}
+           contentLabel="onRequestClose Example"
+           onRequestClose={this.handleCloseModal}
+        >
+          <p>Modal text!</p>
+          <button onClick={this.handleCloseModal}>Close Modal</button>
+        </ReactModal>
           <img className={resultSource + "MovieEntryPoster"} src={posterUrl} width='140'/>
           <div className={resultSource + "MovieInfoContainer"}>
             <div className={resultSource + "ImdbInfo"}>
